fad227e8f7a1fa9c60d09b44db448c41640ebc9d
Marty Bonacci <martybonacci@gmail.com>
2025-08-02 20:12:40 -0600

    added react touter devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouterDevTools } from "react-router-devtools";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouterDevTools(), reactRouter(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
